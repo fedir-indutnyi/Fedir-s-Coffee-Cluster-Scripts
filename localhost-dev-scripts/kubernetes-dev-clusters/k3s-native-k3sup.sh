33caaf47256726dbf4eb41a824ed8ea20955bcc5
Fedir Indutnyi <fedir.indutnyi@gmail.com>
2024-09-20 13:56:38 +0200

    updated scripts

--- localhost-dev-scripts/kubernetes-dev-clusters/k3s-native-k3sup.sh
+++ localhost-dev-scripts/kubernetes-dev-clusters/k3s-native-k3sup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # !!!! Need to be run as "sh k3s-native-k3sup.sh" !!!!
 echo "!!! Important - make sure ending of this file is LF !!!"
+
+echo "Update os"
+sudo apt update
+
 echo "======== Starting to install Localhost Development K3s Prerequisites =================="
 # Define environment variables
 export NODE_NAME="localhost"
@@ -9,99 +13,82 @@ export LOCALHOST_IP="127.0.0.1"
 echo "======== Check Environment Variables =================="
 env
 
+cd ~
+echo "======== Starting k3s installation .. =================="
+export KUBE_EDITOR="nano"
+
+# Add k3sup to PATH
+export PATH=$PATH:$HOME/.k3sup/bin
+
+echo "======== Installing k3s =================="
+
+K3SUP_PATH="./k3sup"  # Specify the path where you want to check for the k3sup file
+# Check if k3sup already exists
+if [ -f "$K3SUP_PATH" ]; then
+    echo "Using existing k3sup binary."
+    K3SUP_CMD="$K3SUP_PATH"
+else
+    echo "Downloading k3sup..."
+    curl -sLS --insecure https://get.k3sup.dev -o k3sup
+    chmod +x k3sup
+    K3SUP_CMD="./k3sup"
+fi
+# Execute k3sup with the desired options
+K3S_KUBECONFIG_MODE="644" $K3SUP_CMD --write-kubeconfig-mode 644
+
+sudo cp k3sup /usr/local/bin/k3sup
+k3sup --help
+echo "======== Running install command =================="
+# k3sup install --local --ip $LOCALHOST_IP --user $USER --k3s-extra-args '--no-deploy traefik'
+k3sup install --local --ip $LOCALHOST_IP --user $USER 
+# this line is important, it allows changing context
+sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
+echo "======== Installation of k3s finished =================="
+
+echo "======== Setting default kubeconfig =================="
+export KUBECONFIG=~/kubeconfig
+# this line doesnt work as this cannot accept ~ for system wide variable .
+# echo "KUBECONFIG=~/kubeconfig" | sudo tee -a /etc/environment
+# this line works locally during startup
+echo 'export KUBECONFIG=~/kubeconfig' >> ~/.bash_profile
+
+export KUBE_EDITOR="nano"
+echo 'KUBE_EDITOR="nano"' | sudo tee -a /etc/environment
+echo "... listing updated environment variables: "
+sudo cat /etc/environment
+sudo cat ~/.bashrc
+kubectl config use-context default
+kubectl get node -o wide
+
+echo "======== Restarting Cluster =================="
+/usr/local/bin/k3s-killall.sh
+sudo systemctl start k3s
+sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
+
+# echo "======== Set variables =================="
+export KUBECONFIG=~/kubeconfig
+sudo cp /etc/rancher/k3s/k3s.yaml ~/kubeconfig
+sudo chmod +rwx ~/kubeconfig
+kubectl config use-context default
+# sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
+kubectl get node -o wide
+# adding default context to user sturtup, possibly not needed if k3s sometimes does it automatically
+echo 'kubectl config use-context default' >> ~/.bash_profile
 
 
+echo "======== Waiting for the node to boot ...  =================="
+until kubectl wait --for condition=ready --timeout=600s node $NODE_NAME; do \
+  sleep 1; \
+done
+kubectl get node -o wide
+kubectl get pods -A
 
-        cd ~
-        echo "======== Starting k3s installation .. =================="
-        export LOCALHOST_IP=$LOCALHOST_IP
-        export SERVER_IP=$LOCALHOST_IP
-        export KUBE_EDITOR="nano"
+echo "======== K3s version =================="
+k3s --v
 
-
-
-        # [ -e kubeconfig ] && rm kubeconfig
-        
-        # echo "======== Downloading k3sup script =================="
-        # # curl -sLS https://get.k3sup.dev | sh
-        # curl -sLS --insecure https://get.k3sup.dev |  K3S_KUBECONFIG_MODE="644" sh -s - --write-kubeconfig-mode 644
-
-        # echo "======== Checking if k3sup script downloaded successfully =================="
-        # chmod +x k3sup
-        # cp ./k3sup ~/k3sup
-        # chmod +x ~/k3sup
-
-        # Add k3sup to PATH
-        export PATH=$PATH:$HOME/.k3sup/bin
-
-        echo "======== Installing k3s =================="
-        
-        K3SUP_PATH="./k3sup"  # Specify the path where you want to check for the k3sup file
-        # Check if k3sup already exists
-        if [ -f "$K3SUP_PATH" ]; then
-            echo "Using existing k3sup binary."
-            K3SUP_CMD="$K3SUP_PATH"
-        else
-            echo "Downloading k3sup..."
-            curl -sLS --insecure https://get.k3sup.dev -o k3sup
-            chmod +x k3sup
-            K3SUP_CMD="./k3sup"
-        fi
-        # Execute k3sup with the desired options
-        K3S_KUBECONFIG_MODE="644" $K3SUP_CMD --write-kubeconfig-mode 644
-
-        sudo cp k3sup /usr/local/bin/k3sup
-        k3sup --help
-        k3sup install --local --ip $LOCALHOST_IP --user $USER --k3s-extra-args '--no-deploy traefik'
-
-                
-        # # sudo install k3sup /usr/local/bin/
-        # bash -c '~/k3sup install --ip $LOCALHOST_IP --user $USER --local'
-        
-        sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
-        echo "======== Installation of k3s finished =================="
-        
-        export KUBECONFIG=/home/$USER/kubeconfig
-        echo "KUBECONFIG=\"/home/$USER/kubeconfig\"" | sudo tee -a /etc/environment
-        export KUBE_EDITOR="nano"
-        echo 'KUBE_EDITOR="nano"' | sudo tee -a /etc/environment
-        echo "... listing updated environment variables: "
-        sudo cat /etc/environment
-        export KUBECONFIG=/home/$USER/kubeconfig
-        kubectl config use-context default
-        kubectl get node -o wide
-        
-        # restart k3s:
-        echo "======== Restarting Cluster =================="
-        /usr/local/bin/k3s-killall.sh
-        sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
-        sudo systemctl start k3s
-        sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
-        echo "======== Trying to get cluster =================="
-        kubectl get node -o wide
-
-        echo "======== Waiting for the node to boot ...  =================="
-        until kubectl wait --for condition=ready --timeout=600s node $NODE_NAME; do \
-          sleep 1; \
-        done
-        kubectl get node -o wide
-        kubectl get pods -A
-        
-        # echo "========Setting final options to prevent permission denied ...  =================="
-        # #this option is useless :(
-        # sudo chmod +rwx /etc/rancher/k3s/k3s.yaml
-        # export KUBECONFIG=/home/$USER/kubeconfig
-        # kubectl config use-context default
-        # sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-        # sudo chown -R $USER:$USER /home/$USER
-
-        echo "======== K3s version =================="
-        k3s --v
-
-        echo "======== Cluster Successfully installed =================="
-        echo "======== For WSL Reboot is needed for Kubectl and variables to take effect =================="
-        echo "to access wsl under windows use : wsl hostname -I "
-
+echo "======== Cluster Successfully installed =================="
+echo "======== For WSL Reboot is needed for Kubectl and variables to take effect =================="
+echo "to access wsl under windows use : wsl hostname -I "
 
 # Verify installation
 k3sup version
